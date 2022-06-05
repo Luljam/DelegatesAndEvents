5b7203172cc48b1eb409e555f04a7e3508726354
Luciano Frutuoso de Brito <britojam@hotmail.com>
2022-06-04 23:00:57 -0300

    Create a custom delegate

--- DelegatesAndEvents/Program.cs
+++ DelegatesAndEvents/Program.cs
@@ -1,19 +1,48 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DelegatesAndEvents
 {
+    // declaração do Delegate
     public delegate void WorkPerformerdHandler(int hours, Worktype worktype);
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            // Instância de delegates como os métodos WorkPerformer
+            WorkPerformerdHandler delegate1 = new WorkPerformerdHandler(WorkPerformed1);
+            WorkPerformerdHandler delegate2 = new WorkPerformerdHandler(WorkPerformed2);
+
+            //delegate1(9, Worktype.GoToMeetings);
+            //delegate2(11, Worktype.GenerateReports);
+
+            DoWork(delegate1);
+
+            Console.Read();
+        }
+
+        static void DoWork(WorkPerformerdHandler del)
+        {
+            // Aqui poderia instanciar o método WorPerformed1 ou 2, não ficaria dinâmico.
+            // Para ficar dinâmico, faço uso do delegate e passo o método que eu quero executar.
+            // Este método (DoWork) desconhece a regra por dentro do método WorPerformed1,
+            // ele apenas passa os valores executa o que foi passado.
+            del(5, Worktype.GoToMeetings);
+        }
+
+        // Método mensagem de Work Performed 1
+        static void WorkPerformed1(int hours, Worktype worktype)
+        {
+            Console.WriteLine("Work Performed 1 called - Hour: {0} Work: {1}", hours, worktype);
+        }
+
+        // Método mensagem de Work Performed 1
+        static void WorkPerformed2(int hours, Worktype worktype)
+        {
+            Console.WriteLine("Work Performed 2 called - Hour: {0} Work: {1}", hours, worktype);
         }
     }
 
+    // Lista de tupos de tarefas
     public enum Worktype
     {
         GoToMeetings,
