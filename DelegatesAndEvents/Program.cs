a28d6db7acb09eb5ff4fef8490ee639f0b70fe01
Luciano Frutuoso de Brito <britojam@hotmail.com>
2022-06-04 22:29:01 -0300

    Add project files.

---
+++ DelegatesAndEvents/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesAndEvents
+{
+    public delegate void WorkPerformerdHandler(int hours, Worktype worktype);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public enum Worktype
+    {
+        GoToMeetings,
+        Golf,
+        GenerateReports
+    }
+}
